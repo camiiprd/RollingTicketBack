cd03ed4b4d303a4103bf1669ba1da6b666de6477
Costilla <lisbetcostilla@gmail.com>
2024-09-28 00:24:57 -0300

    Purchase History

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import userRoutes from './routes/user.routes.js';
 import eventRoutes from './routes/event.routes.js';
 import categoryRoutes from './routes/category.routes.js';
+import purchaseRoutes from './routes/purchase.routes.js';
 import { createRole } from './utils/roles.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
@@ -17,5 +18,6 @@ app.use(cors());
 app.use('/api/usuarios', userRoutes);
 app.use('/api/eventos', eventRoutes);
 app.use('/api/categorias', categoryRoutes);
+app.use('/api/purchases', purchaseRoutes);
 
 export default app;
