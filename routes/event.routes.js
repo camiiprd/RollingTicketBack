7cdf70ad8d556698fd5e62731f4be16b5fc851bc
camiiprd <camila.prado.031@gmail.com>
2024-09-30 01:11:32 -0300

    fix: routes were corrected

--- routes/event.routes.js
+++ routes/event.routes.js
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import { getEvents, getEventById, createEvent, updateEvent, deleteEvent } from '../controllers/event.controller.js';
-import { verifyToken, restrictTo } from '../middlewares/auth.middleware.js';
+
 
 const router = Router();
 
-router.get('/', verifyToken, getEvents);
-router.get('/:id', verifyToken, getEventById);
-router.post('/', verifyToken, restrictTo(['admin', 'event_manager']), createEvent);
-router.put('/:id', verifyToken, restrictTo(['admin', 'event_manager']), updateEvent);
-router.delete('/:id', verifyToken, restrictTo(['admin']), deleteEvent);
+router.get('/', getEvents);
+router.get('/:id', getEventById);
+router.post('/', createEvent);
+router.put('/:id',updateEvent);
+router.delete('/:id',  deleteEvent);
 
 export default router;
