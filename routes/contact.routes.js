b8faa4861986dca77b9923844f8e60cee43e0dc8
JohanaAle <johanaale75@gmail.com>
2024-10-29 22:30:03 -0300

    Added model and contact routes with integration to MongoDB

--- routes/contact.routes.js
+++ routes/contact.routes.js
@@ -1,11 +1,24 @@
-// contact.routes.js
-import express from 'express';
-import { createContact } from '../controllers/contact.controller.js';
+import { Router } from 'express';
+import { Contact } from '../models/contact.model.js'; 
 
-const router = express.Router();
+const router = Router();
 
-router.post('/contact', createContact);
+
+router.post('/contact', async (req, res) => {
+  try {
+    const { name, email, subject, message } = req.body;
+
+    
+    const newContact = new Contact({ name, email, subject, message });
+
+    
+    await newContact.save();
+
+    res.status(201).json({ message: 'Mensaje guardado correctamente.' });
+  } catch (error) {
+    console.error('Error al guardar el mensaje:', error);
+    res.status(500).json({ error: 'Error al guardar el mensaje.' });
+  }
+});
 
 export default router;
-
-// contact.controller.js
